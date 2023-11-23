3b48ff3f2b97f0dc40cc616d3b7cef6898baa90e
Dmitriy Semenov <dsemenov@saec.ru>
2023-11-23 22:45:06 +0500

    second task

--- Program.cs
+++ Program.cs
@@ -2,8 +2,10 @@
 //Console.WriteLine("Hello, World!");
 
 
-Console.Clear();
+
 // Задача 1: Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+/*
+Console.Clear();
 Console.WriteLine("Введите число: ");
 int number = int.Parse(Console.ReadLine()!);
 
@@ -17,4 +19,23 @@ if((number % 7 != 0) & (number % 23 == 0))
 Console.WriteLine("Число не кратно 7, но кратно 23");
 
 if((number % 7 != 0) & (number % 23 != 0))
-Console.WriteLine("Число не кратно  ни 7, ни  23");
+Console.WriteLine("Число не краgiтно  ни 7, ни  23");
+*/
+
+//Задача 2: Напишите программу, которая принимает на вход координаты точки (X и Y), 
+//причём X ≠ 0 и Y ≠ 0 и выдаёт номер координатной четверти плоскости, в которой находится эта точка.
+Console.Clear();
+
+Console.WriteLine("Введите координату X точки: ");
+int X = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите координату Y точки: ");
+int Y = int.Parse(Console.ReadLine()!);
+
+if((X>0) & (Y>0))
+Console.WriteLine($"Точка с координатами ({X},{Y}) находится в 1-й четверте");
+if((X<0) & (Y>0))
+Console.WriteLine($"Точка с координатами ({X},{Y}) находится в 2-й четверте");
+if((X<0) & (Y<0))
+Console.WriteLine($"Точка с координатами ({X},{Y}) находится в 3-й четверте");
+if((X>0) & (Y<0))
+Console.WriteLine($"Точка с координатами ({X},{Y}) находится в 4-й четверте");
